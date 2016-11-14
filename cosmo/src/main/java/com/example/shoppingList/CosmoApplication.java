28fee566617dc2478fb95caa51aabb4c46f22b63
Lode Alaert <lode.alaert@student.odisee.be>
2016-11-14 17:42:23 +0100

    test

---
+++ cosmo/src/main/java/com/example/shoppingList/CosmoApplication.java
@@ -0,0 +1,12 @@
+package com.example.shoppingList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmoApplication.class, args);
+	}
+}
