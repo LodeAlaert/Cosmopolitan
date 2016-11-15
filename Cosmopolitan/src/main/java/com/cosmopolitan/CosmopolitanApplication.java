67fed4c8fa00323c489eac414cfe062c491a4b1d
Lode Alaert <lode.alaert@student.odisee.be>
2016-11-15 04:14:40 +0100

    test

--- Cosmopolitan/src/main/java/com/cosmopolitan/CosmopolitanApplication.java
+++ Cosmopolitan/src/main/java/com/cosmopolitan/CosmopolitanApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CosmopolitanApplication {
 
-	// test of git werkt
 	public static void main(String[] args) {
 		SpringApplication.run(CosmopolitanApplication.class, args);
 	}
