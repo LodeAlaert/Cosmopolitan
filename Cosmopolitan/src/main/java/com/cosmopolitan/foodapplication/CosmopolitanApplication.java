71b900bb363454ba6399b43be7b8d9bce8f3379a
Gert Blanchart <gert.blanchart@student.odisee.be>
2016-11-15 15:51:04 +0100

    fez

--- Cosmopolitan/src/main/java/com/cosmopolitan/foodapplication/CosmopolitanApplication.java
+++ Cosmopolitan/src/main/java/com/cosmopolitan/foodapplication/CosmopolitanApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CosmopolitanApplication {
 
 	// test
-	//test Gert 94
+	//test Gert 2016
 	// foefelen
 	public static void main(String[] args) {
 		SpringApplication.run(CosmopolitanApplication.class, args);
