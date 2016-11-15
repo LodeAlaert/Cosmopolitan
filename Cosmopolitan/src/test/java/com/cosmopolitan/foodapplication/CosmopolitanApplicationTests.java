9e68fb8e805f91e83afb3fe30032a5faa9d94d7c
Olivier Vleminckx <olivier.vleminckx@student.odisee.be>
2016-11-15 14:40:31 +0100

    hoi

--- Cosmopolitan/src/test/java/com/cosmopolitan/foodapplication/CosmopolitanApplicationTests.java
+++ Cosmopolitan/src/test/java/com/cosmopolitan/foodapplication/CosmopolitanApplicationTests.java
@@ -5,13 +5,15 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
+//Dit is een lijntje commentaar
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CosmopolitanApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		//kak
 	}
 
 }
