7004fa130ea76a3c84a07c978a3eaf0c9f6e09cc
Lode Alaert <lode.alaert@student.odisee.be>
2016-11-15 19:54:09 +0100

    Renamed DataEntities -> Models

--- Cosmopolitan/src/main/java/Controllers/CategoryController.java
+++ Cosmopolitan/src/main/java/Controllers/CategoryController.java
@@ -6,14 +6,13 @@
 
 package Controllers;
 
-// imports
-import DataEntities.Category;
-import DataEntities.CategoryDAO;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import Models.Category;
+import Models.CategoryDAO;
 
 @Controller
 public class CategoryController {
--- Cosmopolitan/src/main/java/Models/Category.java
+++ Cosmopolitan/src/main/java/Models/Category.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package DataEntities;
+package Models;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
