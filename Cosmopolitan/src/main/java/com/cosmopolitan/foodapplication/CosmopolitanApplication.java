567810d27074552c7f013ee3dba3d88ac00e7aa4
Lode Alaert <lode.alaert@student.odisee.be>
2016-11-15 15:53:19 +0100

    weg

--- Cosmopolitan/src/main/java/com/cosmopolitan/foodapplication/CosmopolitanApplication.java
+++ Cosmopolitan/src/main/java/com/cosmopolitan/foodapplication/CosmopolitanApplication.java
@@ -6,9 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CosmopolitanApplication {
 
-	// test
-	//test Gert 2016
-	// foefelen
 	public static void main(String[] args) {
 		SpringApplication.run(CosmopolitanApplication.class, args);
 	}
