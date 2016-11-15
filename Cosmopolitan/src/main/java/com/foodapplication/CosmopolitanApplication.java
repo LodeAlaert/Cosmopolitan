362ea6f5f5cd60bb5ab974b132ee27cd90c29885
Lode Alaert <lode.alaert@student.odisee.be>
2016-11-15 04:07:24 +0100

    test

---
+++ Cosmopolitan/src/main/java/com/foodapplication/CosmopolitanApplication.java
@@ -0,0 +1,12 @@
+package com.foodapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmopolitanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmopolitanApplication.class, args);
+	}
+}
