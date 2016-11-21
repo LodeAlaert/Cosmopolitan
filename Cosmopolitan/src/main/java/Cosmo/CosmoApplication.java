93bfd7338f047632d5e7919e4c7ed75a5dfc8240
Lode Alaert <lode.alaert@student.odisee.be>
2016-11-21 14:45:01 +0100

    test for code coverage

--- Cosmopolitan/src/main/java/Cosmo/CosmoApplication.java
+++ Cosmopolitan/src/main/java/Cosmo/CosmoApplication.java
@@ -3,15 +3,19 @@ package Cosmo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import test.CosmoApplicationTest;
+
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan({"Controllers","Repositories"})
+@SpringBootTest(classes = CosmoApplicationTest.class)
 public class CosmoApplication {
 	
 	public static void main(String[] args) {
