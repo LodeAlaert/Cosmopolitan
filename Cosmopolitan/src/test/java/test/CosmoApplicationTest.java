7315da3e39ce1893f7dcccd81f3cbeb7ee8d255f
Lode Alaert <lode.alaert@student.odisee.be>
2016-11-21 15:58:47 +0100

    test

---
+++ Cosmopolitan/src/test/java/test/CosmoApplicationTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath*:spring/applicationContext.xml"})
+public class CosmoApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+}
