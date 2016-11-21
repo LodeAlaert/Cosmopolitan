fa2787ca264b63055a9201907a1bc5532e0e56b4
Lode Alaert <lode.alaert@student.odisee.be>
2016-11-21 20:36:27 +0100

    Added models

--- Cosmopolitan/src/main/java/Models/Category.java
+++ Cosmopolitan/src/main/java/Models/Category.java
@@ -1,5 +1,36 @@
 package Models;
 
 public class Category {
+	
+	// Variables
+	private int category_ID;
+	private String name;
+	
+	// Getters & setters
+	public int getCategory_ID() {
+		return category_ID;
+	}
+	public void setCategory_ID(int category_ID) {
+		this.category_ID = category_ID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	// standard cons
+	public Category(){
+		
+	}
+	
+	// not standard constructor
+	public Category(int category_ID, String name){
+		this.category_ID = category_ID;
+		this.name = name;
+	}
+	
+	
 
 }
--- Cosmopolitan/src/main/java/Models/Recipe.java
+++ Cosmopolitan/src/main/java/Models/Recipe.java
@@ -2,4 +2,96 @@ package Models;
 
 public class Recipe {
 
+	// variables
+	private int recipe_id;
+	private String name;
+	private int difficulty;
+	private int persons;
+	private String description;
+	private String recipe;
+	private String price;
+	private int time;
+
+	// Getters & Setters
+	public int getRecipe_id() {
+		return recipe_id;
+	}
+
+	public void setRecipe_id(int recipe_id) {
+		this.recipe_id = recipe_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(int difficulty) {
+		this.difficulty = difficulty;
+	}
+
+	public int getPersons() {
+		return persons;
+	}
+
+	public void setPersons(int persons) {
+		this.persons = persons;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getRecipe() {
+		return recipe;
+	}
+
+	public void setRecipe(String recipe) {
+		this.recipe = recipe;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public int getTime() {
+		return time;
+	}
+
+	public void setTime(int time) {
+		this.time = time;
+	}
+
+	// standard constructor
+	public Recipe() {
+
+	}
+
+	// not-standard constructor
+	public Recipe(int recipe_id, String name, int difficulty, int persons, String description, String recipe,
+			String price, int time) {
+		this.recipe_id = recipe_id;
+		this.name = name;
+		this.difficulty = difficulty;
+		this.persons = persons;
+		this.description = description;
+		this.recipe = recipe;
+		this.price = price;
+		this.time = time;
+	}
 }
