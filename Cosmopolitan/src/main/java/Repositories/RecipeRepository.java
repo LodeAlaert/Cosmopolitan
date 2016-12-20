62a448d564c2cb928f7a0cd216091bca1faccd73
Gert Blanchart <gert.blanchart@student.odisee.be>
2016-12-20 16:54:23 +0100

    Merge origin/master
    Conflicts:
    	Cosmopolitan/src/main/java/Controllers/RecipeController.java

--- Cosmopolitan/src/main/java/Repositories/RecipeRepository.java
+++ Cosmopolitan/src/main/java/Repositories/RecipeRepository.java
@@ -93,38 +93,41 @@ public class RecipeRepository {
 
     public String GetMatchingrecepies(String searchs) {
 
-       // String[] parts = searchs.split("_");
+        String[] parts = searchs.split("_");
         String search = "";
-        search = searchs.replace("_"," ");
-        
-        // zelfde al anders maar contains op recept (bevat alle ingredienten en veel meer);
-        String query = " SELECT * FROM recipe where recipe like '%" + search + "%' OR name like '%" + search + "%';";
 
-        List<JSONObject> JSONResult = new ArrayList<JSONObject>();
+        for (int i = 0; i < parts.length; i++) {
+            search = parts[i];
+            String query = " SELECT * FROM recipe where recipe like '%" + search + "%' OR name like '%" + search + "%';";
 
-        try {
+            List<JSONObject> JSONResult = new ArrayList<JSONObject>();
 
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://10.129.32.103:3306/cosmo?autoReconnect=true&useSSL=false",
-                    "Cosmo", "Cosmo123");
+            try {
 
-            PreparedStatement ps = conn.prepareStatement(query);
-            ResultSet rs = ps.executeQuery();
-            // to json
-            ResultSetToJSONConverter rstjc = new ResultSetToJSONConverter();
-            JSONResult = rstjc.getFormattedResult(rs);
-            System.out.println(JSONResult);
+                Class.forName("com.mysql.jdbc.Driver");
+                conn = DriverManager.getConnection("jdbc:mysql://10.129.32.103:3306/cosmo?autoReconnect=true&useSSL=false",
+                        "Cosmo", "Cosmo123");
 
-        } catch (Exception e) {
-            System.out.println(e.toString());
+                PreparedStatement ps = conn.prepareStatement(query);
+                ResultSet rs = ps.executeQuery();
+                // to json
+                ResultSetToJSONConverter rstjc = new ResultSetToJSONConverter();
+                JSONResult = rstjc.getFormattedResult(rs);
+                System.out.println(JSONResult);
+
+            } catch (Exception e) {
+                System.out.println(e.toString());
+            }
+            return JSONResult.toString();
+
         }
         return JSONResult.toString();
+        // zelfde al anders maar contains op recept (bevat alle ingredienten en veel meer);
+        // SELECT * FROM cosmo.ingredient inner join recipe_has_ingredient on
+        // Ingredient_ID = recipe_has_ingredient.Ingedient_Ingredient_ID inner join
+        // recipe on recipe_has_ingredient.Recipe_Recipe_ID = recipe.Recipe_ID ;
+        ///SELECT * FROM cosmo.ingredient inner join recipe_has_ingredient on
+        //Ingredient_ID = recipe_has_ingredient.Ingedient_Ingredient_ID inner join
+        // recipe on recipe_has_ingredient.Recipe_Recipe_ID = 1  group by Ingredient_ID ;
     }
-
-    // SELECT * FROM cosmo.ingredient inner join recipe_has_ingredient on
-    // Ingredient_ID = recipe_has_ingredient.Ingedient_Ingredient_ID inner join
-    // recipe on recipe_has_ingredient.Recipe_Recipe_ID = recipe.Recipe_ID ;
-    ///SELECT * FROM cosmo.ingredient inner join recipe_has_ingredient on
-    //Ingredient_ID = recipe_has_ingredient.Ingedient_Ingredient_ID inner join
-    // recipe on recipe_has_ingredient.Recipe_Recipe_ID = 1  group by Ingredient_ID ;
 }
