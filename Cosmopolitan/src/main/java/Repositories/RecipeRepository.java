40ed8e15aeb1991bc563b1cc262ded70271ab3a1
Lode Alaert <lode.alaert@student.odisee.be>
2016-12-20 17:27:27 +0100

    search fix

--- Cosmopolitan/src/main/java/Repositories/RecipeRepository.java
+++ Cosmopolitan/src/main/java/Repositories/RecipeRepository.java
@@ -14,127 +14,143 @@ import Repositories.ResultSetToJSONConverter;
 @Repository
 public class RecipeRepository {
 
-    private static java.sql.Connection conn;
+	private static java.sql.Connection conn;
 
-    List<JSONObject> JSONResult = new ArrayList<JSONObject>();
+	List<JSONObject> JSONResult = new ArrayList<JSONObject>();
 
-    // Fetch all the info needed for the homepage of the application
-    // id, name & description
-    public String FetchAllRecipes() {
+	// Fetch all the info needed for the homepage of the application
+	// id, name & description
+	public String FetchAllRecipes() {
 
-        System.out.println("allRecipes aangeroepen");
+		System.out.println("allRecipes aangeroepen");
 
-        String query = "SELECT recipe_id, name, description FROM recipe";
-        List<JSONObject> JSONResult = new ArrayList<JSONObject>();
+		String query = "SELECT recipe_id, name, description FROM recipe";
+		List<JSONObject> JSONResult = new ArrayList<JSONObject>();
 
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://10.129.32.103:3306/cosmo?autoReconnect=true&useSSL=false",
-                    "Cosmo", "Cosmo123");
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://10.129.32.103:3306/cosmo?autoReconnect=true&useSSL=false",
+					"Cosmo", "Cosmo123");
 
-            PreparedStatement ps = conn.prepareStatement(query);
-            ResultSet rs = ps.executeQuery();
-            // to json
-            ResultSetToJSONConverter rstjc = new ResultSetToJSONConverter();
-            JSONResult = rstjc.getFormattedResult(rs);
+			PreparedStatement ps = conn.prepareStatement(query);
+			ResultSet rs = ps.executeQuery();
+			// to json
+			ResultSetToJSONConverter rstjc = new ResultSetToJSONConverter();
+			JSONResult = rstjc.getFormattedResult(rs);
 
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
-        return JSONResult.toString();
-    }
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		return JSONResult.toString();
+	}
 
-    // fetch a recipe by it's selected ID
-    public String GetRecipeByID(int id) {
-        String query = "SELECT * FROM recipe where Recipe_ID = " + id;
-        // Connection conn = null;
+	// fetch a recipe by it's selected ID
+	public String GetRecipeByID(int id) {
+		String query = "SELECT * FROM recipe where Recipe_ID = " + id;
+		// Connection conn = null;
 
-        List<JSONObject> JSONResult = new ArrayList<JSONObject>();
+		List<JSONObject> JSONResult = new ArrayList<JSONObject>();
 
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://10.129.32.103:3306/cosmo?autoReconnect=true&useSSL=false",
-                    "Cosmo", "Cosmo123");
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://10.129.32.103:3306/cosmo?autoReconnect=true&useSSL=false",
+					"Cosmo", "Cosmo123");
 
-            PreparedStatement ps = conn.prepareStatement(query);
-            ResultSet rs = ps.executeQuery();
+			PreparedStatement ps = conn.prepareStatement(query);
+			ResultSet rs = ps.executeQuery();
 
-            ResultSetToJSONConverter rstjc = new ResultSetToJSONConverter();
-            JSONResult = rstjc.getFormattedResult(rs);
-            System.out.println(JSONResult);
+			ResultSetToJSONConverter rstjc = new ResultSetToJSONConverter();
+			JSONResult = rstjc.getFormattedResult(rs);
+			System.out.println(JSONResult);
 
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
-        return JSONResult.toString();
-    }
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		return JSONResult.toString();
+	}
 
-    public String Filter(String query) {
+	public String Filter(String query) {
 
-        System.out.println(query);
-        List<JSONObject> JSONResult = new ArrayList<JSONObject>();
+		System.out.println(query);
+		List<JSONObject> JSONResult = new ArrayList<JSONObject>();
 
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://10.129.32.103:3306/cosmo?autoReconnect=true&useSSL=false",
-                    "Cosmo", "Cosmo123");
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://10.129.32.103:3306/cosmo?autoReconnect=true&useSSL=false",
+					"Cosmo", "Cosmo123");
 
-            PreparedStatement ps = conn.prepareStatement(query);
-            ResultSet rs = ps.executeQuery();
+			PreparedStatement ps = conn.prepareStatement(query);
+			ResultSet rs = ps.executeQuery();
 
-            ResultSetToJSONConverter rstjc = new ResultSetToJSONConverter();
-            JSONResult = rstjc.getFormattedResult(rs);
+			ResultSetToJSONConverter rstjc = new ResultSetToJSONConverter();
+			JSONResult = rstjc.getFormattedResult(rs);
 
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
-        return JSONResult.toString();
-    }
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		return JSONResult.toString();
+	}
 
-    public String GetMatchingrecepies(String searchs) {
+	public String GetMatchingrecepies(String searchs) {
 
-        String[] parts = searchs.split("_");
-        String search = "";
+		String[] parts = searchs.split("_");
+		String search = "";
+		
+		String recipes = "";
+		String name ="";
+		
+		boolean start = true;
 
-        for (int i = 0; i < parts.length; i++) {
-            search += parts[i];
+		for (int i = 0; i < parts.length; i++) {
 
-            if (i > 0) {
-                search += parts[i] + "OR";
-            }
-            if (i == parts.length) {
-                search += parts[i];
-            }
-        }
+			if (start) {
+				recipes += "'%" + parts[i] + "%'";
+				name += "'%" + parts[i] + "%'";
+				
+				start = false;
+				
+			} else {
+				recipes += " OR '%" + parts[i] + "%'";
+				name += " OR '%" + parts[i] + "%'";
+			}
 
-        System.out.println("DIT     "+ search);
-        String query = " SELECT * FROM recipe where recipe like '%" + search + "%' OR name like '%" + search + "%';";
-        List<JSONObject> JSONResult = new ArrayList<JSONObject>();
+		}
+		
+		
 
-        try {
 
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://10.129.32.103:3306/cosmo?autoReconnect=true&useSSL=false",
-                    "Cosmo", "Cosmo123");
+		String query = " SELECT * FROM recipe where recipe like " + recipes + " OR name like " + name +";";
+		System.out.println(query);
+		List<JSONObject> JSONResult = new ArrayList<JSONObject>();
 
-            PreparedStatement ps = conn.prepareStatement(query);
-            ResultSet rs = ps.executeQuery();
-            // to json
-            ResultSetToJSONConverter rstjc = new ResultSetToJSONConverter();
-            JSONResult = rstjc.getFormattedResult(rs);
-            System.out.println(JSONResult);
+		try {
 
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://10.129.32.103:3306/cosmo?autoReconnect=true&useSSL=false",
+					"Cosmo", "Cosmo123");
 
-        return JSONResult.toString();
-        // zelfde al anders maar contains op recept (bevat alle ingredienten en veel meer);
-        // SELECT * FROM cosmo.ingredient inner join recipe_has_ingredient on
-        // Ingredient_ID = recipe_has_ingredient.Ingedient_Ingredient_ID inner join
-        // recipe on recipe_has_ingredient.Recipe_Recipe_ID = recipe.Recipe_ID ;
-        ///SELECT * FROM cosmo.ingredient inner join recipe_has_ingredient on
-        //Ingredient_ID = recipe_has_ingredient.Ingedient_Ingredient_ID inner join
-        // recipe on recipe_has_ingredient.Recipe_Recipe_ID = 1  group by Ingredient_ID ;
-    }
+			PreparedStatement ps = conn.prepareStatement(query);
+			ResultSet rs = ps.executeQuery();
+			// to json
+			ResultSetToJSONConverter rstjc = new ResultSetToJSONConverter();
+			JSONResult = rstjc.getFormattedResult(rs);
+			System.out.println(JSONResult);
+
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+
+		return JSONResult.toString();
+		// zelfde al anders maar contains op recept (bevat alle ingredienten en
+		// veel meer);
+		// SELECT * FROM cosmo.ingredient inner join recipe_has_ingredient on
+		// Ingredient_ID = recipe_has_ingredient.Ingedient_Ingredient_ID inner
+		// join
+		// recipe on recipe_has_ingredient.Recipe_Recipe_ID = recipe.Recipe_ID ;
+		/// SELECT * FROM cosmo.ingredient inner join recipe_has_ingredient on
+		// Ingredient_ID = recipe_has_ingredient.Ingedient_Ingredient_ID inner
+		// join
+		// recipe on recipe_has_ingredient.Recipe_Recipe_ID = 1 group by
+		// Ingredient_ID ;
+	}
 }
